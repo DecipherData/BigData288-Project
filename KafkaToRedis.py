68bbe87dd993e270e3511230e40d81f96ccb50c4
DecipherData <96799273+DecipherData@users.noreply.github.com>
2024-05-03 13:29:17 -0700

    updated with report and comments

--- KafkaToRedis.py
+++ KafkaToRedis.py
@@ -35,8 +35,10 @@ consumer = KafkaConsumer('user_session_info',
                          bootstrap_servers=['10.0.0.244:29092'],
                          value_deserializer=lambda x: json.loads(x.decode('utf-8')))
 
-# HYPERLOGLOG - HINCRBY is used here to calculate the number of sessions per user. 
-# HYPERLOGLOG is advance technique of Flajolet Martin
+
+# HINCRBY is used here to calculate the number/count of sessions per user. 
+# HINCRBY is a command in Redis used to increment the integer value of a field in a hash stored at a key. 
+# If the key or field does not exist, it is set to 0 before the operation.
 def increment_number_of_sessions(userId):
     """
     Increments the session count for a specific user.
@@ -78,17 +80,28 @@ def process_message(message):
         producer.send('valid_customer_session', message)
         logger.info(f"Existing user {user_id}: Sent to valid_customer_session.")
     else:
-        # If the user does not exist, check device and referrer .. Ideally in real data, this would come after
-        # Bloom filter is applied, but since it is faker data, multiple sessions are generated for each user and devicetype and referrer is 
+        # If the user does not exist, check device and referrer if unknown.. we are assuming that these records may be mallicious
         if device_type == 'unknown' or referrer == 'unknown':
             logger.info(f"Skipped user {user_id} due to unknown device type or referrer.")
             return  # Skip processing this user
-    
+
+        # Valid user is added to the bloom filter
         logger.info(f"Going to add a new user name: {user_name} and Id: {user_id} in the Bloom Filter.")
         r.execute_command('BF.ADD', bloom_filter_name, user_id)
-        r.pfadd(hll_name, user_id) 
+        
+        '''HyperLogLog: An improvement over the Flajolet-Martin algorithm, HyperLogLog (HLL) provides 
+        better accuracy with substantially less memory. It uses multiple hash functions to divide the data 
+        into several registers and applies harmonic mean based calculations to estimate cardinality.
+        Memory Efficiency: HyperLogLog requires significantly less memory (typically around 12 KB) 
+        regardless of the size of the data, making it extremely memory efficient.
+        Precision and Performance: Provides a standard error rate of about 0.81% for 
+        cardinality estimation, which is typically acceptable for many applications, especially given its low memory usage.
+        '''
+        
+        r.pfadd(hll_name, user_id) # also new user entry to hyperloglog.
     
-        # distinct user count
+        # distinct user count using hyperloglog (advance method of flajotlet martin, done in the class)
+        # PFCOUNT is the command used to retrieve the estimated count of unique users from HyperLogLog data structures in Redis.
         estimated_users_ids = r.execute_command('PFCOUNT', hll_name)
         logger.info(f"Unique user Ids: {estimated_users_ids}")
         
